39478e65177d6cd56601a5f1fbfce121d94f1b61
Gregory Liras <gregliras@gmail.com>
2012-06-09 19:57:26 +0300

    updated install script

--- install.sh
+++ install.sh
@@ -2,49 +2,49 @@
 function bash
 {
     echo installing bashrc
-    ln -s bash/bashrc ~/.bashrc
+    ln -s `pwd`/bash/bashrc ~/.bashrc
 }
 function zsh
 {
     echo installing zshrc
-    ln -s zsh/zshrc ~/.zshrc
-    ln -s zsh/zprofile ~/.zprofile
+    ln -s `pwd`/zsh/zshrc ~/.zshrc
+    ln -s `pwd`/zsh/zprofile ~/.zprofile
 }
 function xinit
 {
     echo installing xinitrc
-    ln -s xinit/xinitrc ~/.xinitrc
+    ln -s `pwd`/xinit/xinitrc ~/.xinitrc
 }
 function conky
 {
     echo installing conky config
     mkdir -p ~/.conky
-    ln -s -r conky/conky/* ~/.conky
-    ln -s conky/.conkyrc ~/
+    ln -s `pwd`/conky/conky/* ~/.conky
+    ln -s `pwd`/conky/.conkyrc ~/
 
 }
 function xmonad
 {
     echo installing xmonad config
     mkdir -p ~/.xmonad
-    ln -s xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+    ln -s `pwd`/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
 }
 function vim
 {
     echo installing vim config
-    ln -s -r vim/vim ~/.vim
-    ln -s vim/vimrc ~/.vimrc
+    ln -s `pwd`/vim/vim ~/.vim
+    ln -s `pwd`/vim/vimrc ~/.vimrc
 }
 function openbox
 {
     echo installing openbox config
     mkdir -p ~/.config
-    ln -s -r openbox/openbox ~/.config/
+    ln -s `pwd`/openbox/openbox ~/.config/
 }
 function x
 {
     echo installing Xdefaults
-    ln -s x/Xdefaults ~/.Xdefaults
+    ln -s `pwd`/x/Xdefaults ~/.Xdefaults
 }
 function all
 {
